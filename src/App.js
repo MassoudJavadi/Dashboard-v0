6fbc24834d9cdcf3cdad1e122f171b675fc0f030
Masoud Javadi <77633327+MassoudJavadi@users.noreply.github.com>
2021-11-21 22:55:32 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './app.css';
+import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from './components/navbar/Navbar';
 import Sidebar from './components/sidebar/Sidebar';
